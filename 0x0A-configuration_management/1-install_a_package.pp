5438a972dd751e329c30817d39a736082b383465
Jes <40529386+jestyl@users.noreply.github.com>
2024-05-29 19:01:59 +0000

    fix try

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
-# install a specific version of flask
 #!/usr/bin/puppet
-package{'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  require  => Package['werkzeug'],
+}
+
+package { 'werkzeug':
+  ensure   => '2.0.3', # This version is compatible with Flask 2.1.0
+  provider => 'pip3',
 }
